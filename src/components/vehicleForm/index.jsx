52dec71037beb700d1413dc73c5c1330d16cf8d2
Christian <bournecreative@gmail.com>
2023-12-31 14:05:31 -0600

    wire up name and cost inputs to  reducers from form slice

--- src/components/vehicleForm/index.jsx
+++ src/components/vehicleForm/index.jsx
@@ -4,16 +4,28 @@ import { changeName, changeCost, addVehicle } from "../../store/index";
 export const VehicleForm = () => {
   const dispatch = useDispatch();
 
-  const name = useSelector((state) => state.form.name);
+  const { name, cost } = useSelector((state) => state.form);
+
   const handleNameChange = (e) => {
     dispatch(changeName(e.target.value));
+  };
+
+  const handleCostChange = (e) => {
+    dispatch(changeCost(parseInt(e.target.value) || 0));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addVehicle({ name: name, cost: cost }));
+    dispatch(changeName(""));
+    dispatch(changeCost(""));
   };
 
   return (
     <>
       <div className="car-form panel">
         <h4 className="subtitle is-3">Add Vehicle</h4>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="field-group">
             <div className="field">
               <label className="label">Vehicle Name</label>
@@ -22,7 +34,18 @@ export const VehicleForm = () => {
                 value={name}
                 onChange={handleNameChange}
               />
+              <label className="label">Vehicle Cost</label>
+              <input
+                className="input is-expanded"
+                value={cost || ""}
+                onChange={handleCostChange}
+              />
             </div>
+          </div>
+          <div className="field">
+            <button type="submit" className="button is-link">
+              Submit
+            </button>
           </div>
         </form>
       </div>
