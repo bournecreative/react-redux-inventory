4e8c698747fcdec0fe40fd08593c5791b5188268
Christian <bournecreative@gmail.com>
2024-01-01 20:32:25 -0600

    use vehicle list and search term to create dervived state to provide vehicle cost. If search is engage reflect total related only to searched term

--- src/components/vehicleValue/index.jsx
+++ src/components/vehicleValue/index.jsx
@@ -1,3 +1,14 @@
+import { useSelector } from "react-redux";
+
 export const VehicleValue = () => {
-  return <div>VehicleValue</div>;
+  const total = useSelector(({ vehicles: { list, searchTerm } }) => {
+    let carTotal = 0;
+    list.filter((v) => {
+      if (v.name.toLowerCase().match(searchTerm.toLowerCase())) {
+        carTotal = carTotal + v.cost;
+      }
+    });
+    return carTotal;
+  });
+  return <div className="car-value">${total}</div>;
 };
